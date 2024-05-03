e613a93706c754d6fcf0ba3283aab4efd58ea72d
Rodolfo Scarp <rodolfo@jrcontabiltr.com.br>
2024-05-03 11:14:20 -0300

    infra: adicionado arquivo .spec infra: adicionado arquivo de loadin pyinstaller

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 from src.desktop import show_screen
+from src.infraestructure import loading
 
 
 def exec_app():
+    loading()
     show_screen()
---
+++ src/infraestructure/loading.py
@@ -0,0 +1,11 @@
+# pylint: disable=E0401
+import os
+import importlib
+
+
+def loading():
+    if "_PYIBoot_SPLASH" in os.environ and importlib.util.find_spec("pyi_splash"):
+        import pyi_splash
+
+        pyi_splash.update_text("Carregando...")
+        pyi_splash.close()
